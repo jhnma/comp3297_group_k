9fc1c6fc88d11f7c4e619d32a5720f4c3cdf0af3
Nelson Pang <chnelsonpang@gmail.com>
2020-11-20 00:07:15 +0800

    change login page url
    conflict in hotzone_config/urls.py

--- hotzone_project/hotzone_config/urls.py
+++ hotzone_project/hotzone_config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cases/', include('cases.urls')),
-    path('', include('django.contrib.auth.urls')),
+    path('account/', include('django.contrib.auth.urls')),
 ]
