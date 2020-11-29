93731df37d2440d38ff98403f26317bd954afd01
terry <terry331234@protonmail.com>
2020-11-29 13:59:17 +0800

    remove unnecessary code

--- hotzone_project/cluster/views.py
+++ hotzone_project/cluster/views.py
@@ -1,20 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import View
 from django.http import HttpResponse
 
 # Create your views here.
-class Cluster(TemplateView):
+class Cluster(View):
     template_name = "cluster.html"
 
     def get(self, request, **kwargs):
-        if request.method == 'GET':
-            c = request.GET.get('c', 0)
-            d = request.GET.get('d', 0)
-            t = request.GET.get('t', 0)
-            if (c and d and t):
-                context = {'d':d, 'c':c, 't':t}
-                return render(request, self.template_name, context)
-            else:
-                return render(request, self.template_name, {})
+        c = request.GET.get('c', 0)
+        d = request.GET.get('d', 0)
+        t = request.GET.get('t', 0)
+        if (c and d and t):
+            context = {'d':d, 'c':c, 't':t}
+            return render(request, self.template_name, context)
         else:
-            return HttpResponse("Bad Request")
+            return render(request, self.template_name, {})
