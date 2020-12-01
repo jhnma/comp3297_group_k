13045cb1a9d837cb6ce03e83517427fb94ebd2b4
terry <terry331234@protonmail.com>
2020-12-01 15:25:35 +0800

    fix wrong case number

--- hotzone_project/cluster/views.py
+++ hotzone_project/cluster/views.py
@@ -13,34 +13,34 @@ class Cluster(View):
     template_name = "cluster.html"
 
     def get(self, request, *args, **kwargs):
-        try:
-            visits = list(Visit.objects.values('id', 'location_id', 'date_from'))
-        except ObjectDoesNotExist:
-            return HttpResponseNotFound("Case doesn't exist.")
-        
-        vector = []
-
-        for visit in visits:
-            vector.append([
-                list(Location.objects.values('x').filter(id=visit['location_id']))[0]['x'],
-                list(Location.objects.values('y').filter(id=visit['location_id']))[0]['y'],
-                (visit['date_from'] - datetime.date(2020, 1, 1)).days,
-                visit['id']
-            ])
-        
-        vector = np.array(vector)
-        return render(request, self.template_name, {'vector': vector})
+        return render(request, self.template_name, {})
 
     def post(self, request):
         c = request.POST.get('c', 0)
         d = request.POST.get('d', 0)
         t = request.POST.get('t', 0)
-        #print(c, d, t)
 
-        # TODO: Clustering
         if (c and d and t):
+            try:
+                visits = list(Visit.objects.values('case', 'location_id', 'date_from'))
+            except ObjectDoesNotExist:
+                return HttpResponseNotFound("Case doesn't exist.")
+            
+            vector = []
+
+            for visit in visits:
+                vector.append([
+                    list(Location.objects.values('x').filter(id=visit['location_id']))[0]['x'],
+                    list(Location.objects.values('y').filter(id=visit['location_id']))[0]['y'],
+                    (visit['date_from'] - datetime.date(2020, 1, 1)).days,
+                    int(Case.objects.filter(id=visit['case']).values('case_id')[0]['case_id'])
+                ])
+            
+            vector = np.array(vector)
+            # TODO: doClustering(vector, int(d), int(t), int(c))
             cluster_result = {}
             context = {'d':d, 'c':c, 't':t, 'cluster_result': cluster_result}
             return render(request, self.template_name, context)
+
         else:
             return render(request, self.template_name, {})
